e350889f813b3f21122b4117e28575a4989db2a2
Jared <jared.bardales@gmail.com>
2025-07-18 16:22:09 -0500

    fix: errores corregidos para despliegue

--- src/handlers/follow.ts
+++ src/handlers/follow.ts
@@ -4,7 +4,7 @@ import User from '../models/User'
 
 interface AuthUser {
   _id: ObjectId
-  following: unknown[] // TypeScript lo ve así
+  following: ObjectId[]
 }
 
 export const followUser = async (req: Request, res: Response) => {
@@ -15,25 +15,28 @@ export const followUser = async (req: Request, res: Response) => {
     const target = await User.findOne({ handle })
     if (!target) return res.status(404).json({ error: 'Usuario objetivo no encontrado.' })
 
-    if (user._id.equals(target._id)) {
+    const userId = user._id as ObjectId
+    const targetId = target._id as ObjectId
+
+    if (userId.equals(targetId)) {
       return res.status(400).json({ error: 'No puedes seguirte a ti mismo.' })
     }
 
     const followingIds = user.following as ObjectId[]
     const followerIds = target.followers as ObjectId[]
 
-    const isFollowing = followingIds.some(id => id.equals(target._id))
+    const isFollowing = followingIds.some(id => id.equals(targetId))
 
     if (isFollowing) {
-      user.following = followingIds.filter(id => !id.equals(target._id))
-      target.followers = followerIds.filter(id => !id.equals(user._id))
+      user.following = followingIds.filter(id => !id.equals(targetId))
+      target.followers = followerIds.filter(id => !id.equals(userId))
     } else {
-      user.following.push(target._id)
-      target.followers.push(user._id)
+      user.following.push(targetId)
+      target.followers.push(userId)
     }
 
-    await User.findByIdAndUpdate(user._id, { following: user.following })
-    await User.findByIdAndUpdate(target._id, { followers: target.followers })
+    await User.findByIdAndUpdate(userId, { following: user.following })
+    await User.findByIdAndUpdate(targetId, { followers: target.followers })
 
     res.json({
       message: isFollowing ? 'Usuario dejado de seguir.' : 'Usuario seguido con éxito.',
